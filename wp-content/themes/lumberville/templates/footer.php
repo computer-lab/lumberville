d6aba94805d8ac4dd6e5470d9b047df60b0925e2
Patrick Steadman <ptsteadman@gmail.com>
2016-04-03 18:40:32 -0400

    Rename theme

--- wp-content/themes/lumberville/templates/footer.php
+++ wp-content/themes/lumberville/templates/footer.php
@@ -3,3 +3,5 @@
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
+<script src="https://use.typekit.net/rct5qgp.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
